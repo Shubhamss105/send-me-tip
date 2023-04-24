3cf2b507dcc423092792b9996753bebc371b8553
Shubhamss105 <shubhamsingh.ss.1407@gmail.com>
2023-04-25 02:16:45 +0530

    send me tip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from 'ethers';
+import {useEffect, useState} from 'react';
+import abi from './contract/chai.json'
+import Buy from './components/Buy';
+import Memos from './components/Memos';
+
 
 function App() {
+
+  const [state,setState] = useState({
+    provider:null,
+    signer:null,
+    contract:null
+  })
+
+  useEffect(()=>{
+    const connectWallet = async()=>{
+      const contractAddress= '0x913865f309bda54ba679e72308d4a82572f36531';
+      const contractAbi = abi.abi;
+      try{
+        const {ethereum} = window;
+
+        if(ethereum){
+          const accounts = await ethereum.request({method:"eth_requestAccounts"});
+          console.log(accounts);
+        }
+        const provider= new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress,contractAbi, signer);
+        setState({provider,signer,contract});
+
+      }catch(error){
+        console.log(error);
+      }
+    }
+    connectWallet();
+  },[])
+
+  console.log(state);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Buy state={state}/>
+      <Memos state={state}/>
     </div>
   );
 }
